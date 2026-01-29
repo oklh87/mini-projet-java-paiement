b535ccfb2374eecb6f9be6799b6219ac422edc46
Amer Bella <amerbella678@gmail.com>
2026-01-29 08:22:00 +0100

    Update FactureNonTrouveeException.java

--- src/ma/fste/miniprojet/exception/FactureNonTrouveeException.java
+++ src/ma/fste/miniprojet/exception/FactureNonTrouveeException.java
@@ -1,8 +1,22 @@
 package ma.fste.miniprojet.exception;
 
+/**
+ * Exception personnalisée levée lorsqu'une facture
+ * recherchée n'est pas trouvée dans le système.
+ *
+ * Elle permet de gérer proprement les erreurs métier
+ * au lieu d'utiliser des exceptions génériques.
+ */
 public class FactureNonTrouveeException extends Exception {
+
+    /**
+     * Constructeur de l'exception avec un message explicatif.
+     *
+     * @param message Message décrivant la cause de l'erreur
+     */
     public FactureNonTrouveeException(String message) {
         super(message);
     }
 }
+
 
