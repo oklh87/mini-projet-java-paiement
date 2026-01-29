005cae45ffb6b7b646623fd7237091338169f680
Amer Bella <amerbella678@gmail.com>
2026-01-29 08:25:27 +0100

    Update Paiement.java

--- src/ma/fste/miniprojet/model/Paiement.java
+++ src/ma/fste/miniprojet/model/Paiement.java
@@ -1,15 +1,36 @@
 package ma.fste.miniprojet.model;
 
+/**
+ * Classe représentant un Paiement.
+ * Hérite de la classe Transaction et ajoute
+ * l'information sur la méthode de paiement utilisée.
+ */
 public class Paiement extends Transaction {
+
+    // Méthode de paiement (ex: Carte, Cash, Virement...)
     private String methode;
 
+    /**
+     * Constructeur de la classe Paiement.
+     *
+     * @param id Identifiant unique du paiement
+     * @param montant Montant du paiement
+     * @param methode Méthode de paiement utilisée
+     */
     public Paiement(int id, double montant, String methode) {
-        super(id, montant);
+        super(id, montant); // Initialisation des champs hérités
         this.methode = methode;
     }
 
+    /**
+     * Retourne une représentation textuelle du paiement,
+     * incluant les informations de base et la méthode utilisée.
+     *
+     * @return Description du paiement
+     */
     @Override
     public String toString() {
         return super.toString() + " | Méthode: " + methode;
     }
 }
+
