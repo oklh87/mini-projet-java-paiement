e7647fa0c8755732cf69a40238fad57ff6e3ace1
Amer Bella <amerbella678@gmail.com>
2026-01-29 08:16:54 +0100

    Update FactureDAO.java

--- src/ma/fste/miniprojet/dao/FactureDAO.java
+++ src/ma/fste/miniprojet/dao/FactureDAO.java
@@ -1,8 +1,29 @@
 package ma.fste.miniprojet.dao;
+
 import java.util.List;
 
-    public interface FactureDAO {
-           void save(List<?> data);
-           List<?> load();
-    }
+/**
+ * Interface DAO (Data Access Object) pour la gestion
+ * de la persistance des factures.
+ *
+ * Elle définit les méthodes nécessaires pour
+ * sauvegarder et charger des données depuis un support
+ * (fichier, base de données, mémoire, etc.).
+ */
+public interface FactureDAO {
+
+    /**
+     * Sauvegarde une liste de données (factures) vers un support de stockage.
+     *
+     * @param data Liste d'objets à sauvegarder
+     */
+    void save(List<?> data);
+
+    /**
+     * Charge la liste des données (factures) depuis un support de stockage.
+     *
+     * @return Liste d'objets chargés
+     */
+    List<?> load();
+}
 
