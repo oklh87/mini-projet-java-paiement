3ed9b61f81269f330eb96631b5b3bb63bb9de1db
Amer Bella <amerbella678@gmail.com>
2026-01-29 08:30:35 +0100

    Update ValidationUtil.java

--- src/ma/fste/miniprojet/util/ValidationUtil.java
+++ src/ma/fste/miniprojet/util/ValidationUtil.java
@@ -1,8 +1,24 @@
 package ma.fste.miniprojet.util;
 
+/**
+ * Classe utilitaire pour valider certaines données
+ * du projet, comme les montants des factures ou paiements.
+ * 
+ * Les méthodes sont statiques pour pouvoir être appelées
+ * directement sans créer d'objet.
+ */
 public class ValidationUtil {
+
+    /**
+     * Vérifie si un montant est valide.
+     * Un montant est considéré valide s'il est strictement positif.
+     *
+     * @param montant Montant à vérifier
+     * @return true si le montant > 0, false sinon
+     */
     public static boolean montantValide(double montant) {
         return montant > 0;
     }
 }
+
 
